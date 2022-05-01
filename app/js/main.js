5226a904894b06480a77aa07b3f1867541409b37
ekaterynakravchuk <ekateryna.kravchuk@gmail.com>
2022-05-01 11:49:46 +0300

    device section

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,5 @@
 $(function() {
-    $('.header__lang-link').on('click', function(event) {
-        event.preventDefault();
+    $('.header__lang-link').on('click', function() {
         $('.header__lang-link').toggleClass('header__lang-link--active');
     })
   
