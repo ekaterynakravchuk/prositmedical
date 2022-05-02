c5cc2ef1fecd82139f3aa5da0738cec85f9dd620
ekaterynakravchuk <ekateryna.kravchuk@gmail.com>
2022-05-02 18:42:14 +0300

    popup

--- app/js/main.js
+++ app/js/main.js
@@ -2,5 +2,22 @@ $(function() {
     $('.header__lang-link').on('click', function() {
         $('.header__lang-link').toggleClass('header__lang-link--active');
     })
+
+    $('.device__price-btn').on('click', function(event) {
+        event.preventDefault();
+        $('.popup__order').addClass('order__open');
+    })
+
+    $('.popup__btn').on('click', function(event) {
+        event.preventDefault();
+        $('.popup__order').removeClass('order__open');
+        $('.popup__feedback').addClass('open__feedback');
+    })
+
+    $('.popup__close').on('click', function(event) {
+        event.preventDefault();
+        $('.popup__order').removeClass('order__open');
+        $('.popup__feedback').removeClass('open__feedback');
+    })
   
 })
